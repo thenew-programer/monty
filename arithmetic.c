3d8ef7df905ddc6762ec10fc679d7d6e7c3eef28
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-25 09:45:56 +0100

    div and mul function

--- arithmetic.c
+++ arithmetic.c
@@ -54,3 +54,64 @@ void do_sub(stack_t **stack, unsigned int line_number)
 
 	(*stack)->n = result;
 }
+
+
+/**
+ * do_div - function that dividthe first two elements of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: error line number
+ *
+ * Return: void
+ */
+void do_div(stack_t **stack, unsigned int line_number)
+{
+	int a, b, result;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	b = (*stack)->n;
+	a = (*stack)->next->n;
+	if (b == 0)
+	{
+		fprintf(stderr, "L%u: division by zero", line_number);
+		exit(EXIT_FAILURE);
+	}
+	result = a / b;
+
+	do_pop(stack, line_number);
+
+	(*stack)->n = result;
+}
+
+
+/**
+ * do_mul - function that multiply the first two elements of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: error line number
+ *
+ * Return: void
+ */
+void do_mul(stack_t **stack, unsigned int line_number)
+{
+	int a, b, result;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	b = (*stack)->n;
+	a = (*stack)->next->n;
+	result = a * b;
+
+	do_pop(stack, line_number);
+
+	(*stack)->n = result;
+}
