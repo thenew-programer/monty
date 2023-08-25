cf0bf114aa4c607a331b80ce8f1cd9edb5832168
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-25 08:49:39 +0100

    betty stuff

--- stack_instructions.c
+++ stack_instructions.c
@@ -140,15 +140,3 @@ void do_swap(stack_t **stack, unsigned int line_number)
 
 }
 
-/**
- * do_nop - opcode that does nothing
- * @stack: pointer to stack
- * @line_number: line number
- *
- * Return: void
- */
-void do_nop(stack_t **stack, unsigned int line_number)
-{
-	(void)stack;
-	(void)line_number;
-}
