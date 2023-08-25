42ab4ced5f384a5edabbeca41356d979e788b5e6
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-25 08:50:04 +0100

    moved nop func to another file for pleasing betty

---
+++ stack_ins_2.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * do_nop - opcode that does nothing
+ * @stack: pointer to stack
+ * @line_number: line number
+ *
+ * Return: void
+ */
+void do_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
